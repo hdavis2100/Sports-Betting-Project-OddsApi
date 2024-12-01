61f067a117deca1da7e0c15c59e947b4b59414bf
Harutune Davis <d.harutune@wustl.edu>
2024-11-30 23:23:09 -0600

    basically done with header. Created Logo in canva. chose body, header background. Made actionable signup content and contrasted it.

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import wustlLogo from './assets/wustl.svg'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <div>
-        <a href="https://wustl.edu" target="_blank" rel="noreferrer">
-          <img src={wustlLogo} className="logo wustl" alt="WUSTL logo" />
-        </a>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>WUSTL + Vite + React</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the WUSTL, Vite and React logos to learn more
-      </p>
+      <header className="header">
+        <div className="logo-container">
+          <img src="/logo2.png" alt="BETMO Logo" className="logo" />
+          
+        </div>
+        <div className="nav">
+        <nav>
+          <a href="#">Home</a>
+          <a href="#">About</a>
+          <a href="#">Contact</a>
+          
+        </nav>
+        <div id="action">
+          <a href="#">Login</a>
+          <a href="#" className="signup">Sign Up</a>
+        </div>
+        </div>
+        
+      </header>
     </>
   )
 }
+
 
 export default App
